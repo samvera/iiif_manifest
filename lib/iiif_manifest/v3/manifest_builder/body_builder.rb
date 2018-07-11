78f34eb26b21b2e1a792fc8c0bbf02513ac7aecd
yingfeng-iu <yingfeng@iu.edu>
2018-07-11 16:08:20 -0400

    Issue langmap: add nil checking to language_map
    Issue langmap: fix language map issue when label etc is nil or empty
    
    Don't let nil values through to the generated json.
    
    Co-authored-by: yingfeng-iu <yingfeng@iu.edu>
    
    Appease rubocop
    
    Issue#116 follwoup: fix bugs in language map, add nil checking before adding fields to hash
    Co-authored-by: Chris Colvard <cjcolvar@iu.edu>
    
    Issue#116 follwoup: fix robocop
    
    Fix rubocop issues and ensure values are present
    
    Co-authored-by: Chris Colvard <cjcolvar@indiana.edu>

--- lib/iiif_manifest/v3/manifest_builder/body_builder.rb
+++ lib/iiif_manifest/v3/manifest_builder/body_builder.rb
@@ -20,11 +20,11 @@ module IIIFManifest
           def build_body
             body['id'] = display_content.url
             body['type'] = body_type
-            body['height'] = display_content.height if display_content.try(:height)
-            body['width'] = display_content.width if display_content.try(:width)
-            body['duration'] = display_content.duration if display_content.try(:duration)
-            body['format'] = display_content.format if display_content.format
-            body['label'] = ManifestBuilder.language_map(display_content.label) if display_content.try(:label)
+            body['height'] = display_content.height if display_content.try(:height).present?
+            body['width'] = display_content.width if display_content.try(:width).present?
+            body['duration'] = display_content.duration if display_content.try(:duration).present?
+            body['format'] = display_content.format if display_content.try(:format).present?
+            body['label'] = ManifestBuilder.language_map(display_content.label) if display_content.try(:label).present?
           end
 
           def body
--- spec/lib/iiif_manifest/v3/manifest_builder/structure_builder_spec.rb
+++ spec/lib/iiif_manifest/v3/manifest_builder/structure_builder_spec.rb
@@ -114,5 +114,69 @@ RSpec.describe IIIFManifest::V3::ManifestBuilder::StructureBuilder do
         expect(top_range['items'][2]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Back-Cover'
       end
     end
+
+    context 'with language maps' do
+      let(:ranges) do
+        [
+          ManifestRange.new(label: { '@none' => ['Table of Contents'] }, items: [
+                              double(id: 'Front-Cover'),
+                              ManifestRange.new(label: { '@none' => ['Chapter 1'] }, items: [
+                                                  double(id: 'Page-1'),
+                                                  double(id: 'Page-2')
+                                                ]),
+                              ManifestRange.new(items: [
+                                                  double(id: 'Page-3'),
+                                                  double(id: 'Page-4')
+                                                ]),
+                              double(id: 'Back-Cover')
+                            ])
+        ]
+      end
+      let(:json_result) { JSON.parse(subject.to_json) }
+
+      before do
+        class ManifestRange
+          attr_reader :label, :items
+          def initialize(label: nil, items: [])
+            @label = label
+            @items = items
+          end
+        end
+      end
+
+      after do
+        Object.send(:remove_const, :ManifestRange)
+      end
+
+      it 'builds the structure' do
+        subject
+        top_range = manifest['structures'].first
+        expect(top_range['label']).to eq('@none' => ['Table of Contents'])
+        expect(top_range['behavior']).to eq 'top'
+        expect(top_range['items'].length).to eq 4
+        expect(top_range['items'][0]['type']).to eq 'Canvas'
+        expect(top_range['items'][0]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Front-Cover'
+        sub_range = top_range['items'][1]
+        expect(sub_range['type']).to eq 'Range'
+        expect(sub_range['label']).to eq('@none' => ['Chapter 1'])
+        expect(sub_range['items'].length).to eq 2
+        expect(sub_range['items'][0]['type']).to eq 'Canvas'
+        expect(sub_range['items'][0]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Page-1'
+        expect(sub_range['items'][1]['type']).to eq 'Canvas'
+        expect(sub_range['items'][1]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Page-2'
+        sub_range2 = top_range['items'][2]
+        expect(sub_range2['type']).to eq 'Range'
+        expect(sub_range2['label']).to be_nil
+        expect(sub_range2['items'].length).to eq 2
+        expect(sub_range2['items'][0]['type']).to eq 'Canvas'
+        expect(sub_range2['items'][0]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Page-3'
+        expect(sub_range2['items'][1]['type']).to eq 'Canvas'
+        expect(sub_range2['items'][1]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Page-4'
+        expect(top_range['items'][3]['type']).to eq 'Canvas'
+        expect(top_range['items'][3]['id']).to eq 'http://test.host/books/book-77/manifest/canvas/Back-Cover'
+        # Check that the empty label doesn't come through to the json
+        expect(json_result['structures'][0]['items'][2].key?('label')).to be false
+      end
+    end
   end
 end
