042bebaa051e2b457510d389f230c90c7eb9102c
Kirk Wang <kirk.wang@scientist.com>
2022-09-28 15:39:04 -0700

    disable Metrics/PerceivedComplexity for #setup_manifest_from_record

--- lib/iiif_manifest/v3/manifest_builder/record_property_builder.rb
+++ lib/iiif_manifest/v3/manifest_builder/record_property_builder.rb
@@ -41,7 +41,7 @@ module IIIFManifest
           canvas_builder_factory.from(record)
         end
 
-          # rubocop:disable Metrics/CyclomaticComplexity
+          # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
         def setup_manifest_from_record(manifest, record)
           manifest['id'] = record.manifest_url.to_s
           manifest.label = ManifestBuilder.language_map(record.to_s) if record.to_s.present?
@@ -54,7 +54,7 @@ module IIIFManifest
           manifest.rendering = populate_rendering if populate_rendering.first.present?
           manifest.homepage = record.homepage if record.try(:homepage).present?
         end
-          # rubocop:enable Metrics/CyclomaticComplexity
+          # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
         def metadata_from_record(record)
           if valid_v3_metadata?
