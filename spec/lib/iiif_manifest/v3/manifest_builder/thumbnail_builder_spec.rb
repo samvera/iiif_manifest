c38e517b062146c749394d2200b47ab8ba34434e
Kirk Wang <kirk.wang@scientist.com>
2022-10-12 23:00:39 -0700

    add more tests to thumbnail builder spec

--- spec/lib/iiif_manifest/v3/manifest_builder/thumbnail_builder_spec.rb
+++ spec/lib/iiif_manifest/v3/manifest_builder/thumbnail_builder_spec.rb
@@ -9,19 +9,61 @@ RSpec.describe IIIFManifest::V3::ManifestBuilder::ThumbnailBuilder do
       image_service_builder_factory: image_service_builder_factory
     )
   end
-  let(:url) { 'http://example.com/img1/full/600,/0/default.jpg' }
-  let(:display_content) { IIIFManifest::DisplayImage.new(url, width: 640, height: 480) }
+  let(:url) { 'http://example.com/img1' }
+  let(:iiif_endpoint) { double }
+  let(:display_content) do
+    IIIFManifest::DisplayImage.new(
+      url,
+      width: 640,
+      height: 480,
+      iiif_endpoint: iiif_endpoint,
+      format: 'Image'
+    )
+  end
   let(:canvas) { IIIFManifest::V3::ManifestBuilder::IIIFManifest::Canvas.new }
   let(:image_service_builder_factory) { IIIFManifest::V3::ManifestServiceLocator.image_service_builder_factory }
 
-  describe '#apply' do
-    subject(:thumbnail_builder) { builder.apply(canvas) }
+  before do
+    allow(iiif_endpoint).to receive(:url).and_return(url)
+    builder.apply(canvas)
+  end
 
-    context 'without iiif_endpoint' do
-      it 'sets a thumbnail on the canvas' do
-        thumbnail_builder
-        expect(canvas.thumbnail).to be_kind_of IIIFManifest::V3::ManifestBuilder::IIIFManifest::Thumbnail
+  describe '#apply' do
+    it 'sets a thumbnail on the canvas' do
+      expect(canvas.thumbnail).to be_a IIIFManifest::V3::ManifestBuilder::IIIFManifest::Thumbnail
+    end
+  end
+
+  describe '#build_thumbnail' do
+    it 'sets properties on the thumbnail' do
+      expect(canvas.thumbnail['type']).to eq 'Image'
+      expect(canvas.thumbnail['id']).to eq url + '/full/!200,200/0/default.jpg'
+      expect(canvas.thumbnail['width']).to eq 200
+      expect(canvas.thumbnail['height']).to eq 150
+    end
+  end
+
+  describe '#reduction_ratio' do
+    context 'when the content is wider' do
+      it 'creates a multiplier based off the max width of 200' do
+        allow(display_content).to receive(:width).and_return(2000)
+        allow(display_content).to receive(:height).and_return(1000)
+        expect(builder.send(:reduction_ratio)).to eq 0.1
       end
+    end
+
+    context 'when the content is taller' do
+      it 'creates a multiplier based off the max height of 200' do
+        allow(display_content).to receive(:width).and_return(1000)
+        allow(display_content).to receive(:height).and_return(2000)
+        expect(builder.send(:reduction_ratio)).to eq 0.1
+      end
+    end
+  end
+
+  describe '#thumbnail' do
+    it 'creates a new thumbnail' do
+      expect(builder.send(:thumbnail)).to be_a IIIFManifest::V3::ManifestBuilder::IIIFManifest::Thumbnail
     end
   end
 end
