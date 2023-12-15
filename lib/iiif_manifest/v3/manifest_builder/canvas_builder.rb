2141165b586431eb32de28d21801192986e18e52
Chris Colvard <chris.colvard@gmail.com>
2023-12-15 13:31:59 -0500

    Update canvas_builder.rb

--- lib/iiif_manifest/v3/manifest_builder/canvas_builder.rb
+++ lib/iiif_manifest/v3/manifest_builder/canvas_builder.rb
@@ -57,8 +57,8 @@ module IIIFManifest
         # @return [Array<Object>] if the record has a display content
         # @return [NilClass] if there is no display content
         def display_content
-          return @display_content if @display_content.present?
-          return unless record.respond_to?(:display_content) && record.display_content.present?
+          return @display_content unless @display_content.nil?
+          return unless record.respond_to?(:display_content) && !record.display_content.nil?
 
           @display_content ||= Array.wrap(record.display_content)
         end
@@ -66,8 +66,8 @@ module IIIFManifest
         # @return [Array<Object>] if the record has generic annotation content
         # @return [NilClass] if there is no annotation content
         def annotation_content
-          return @annotation_content if @annotation_content.present?
-          return unless record.respond_to?(:annotation_content) && record.annotation_content.present?
+          return @annotation_content unless @annotation_content.nil?
+          return unless record.respond_to?(:annotation_content) && !record.annotation_content.nil?
 
           @annotation_content ||= Array.wrap(record.annotation_content)
         end
